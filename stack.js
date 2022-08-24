bee0711c630a803847301308d33dd9b244c1d598
Jess Stayton <jessica.stayton@gmail.com>
2022-08-23 18:14:11 -0600

    committing completed exercise

--- stack.js
+++ stack.js
@@ -1,46 +1,67 @@
 /** Node: node for a stack. */
 
 class Node {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
-  }
+	constructor(val) {
+		this.val = val;
+		this.next = null;
+	}
 }
 
 /** Stack: chained-together nodes where you can
  *  remove from the top or add to the top. */
 
 class Stack {
-  constructor() {
-    this.first = null;
-    this.last = null;
-    this.size = 0;
-  }
+	constructor() {
+		this.first = null;
+		this.last = null;
+		this.size = 0;
+	}
 
-  /** push(val): add new value to end of the stack. Returns undefined. */
+	/** push(val): add new value to end of the stack. Returns undefined. */
 
-  push(val) {
+	push(val) {
+		let node = new Node(val);
+		if (!this.first) {
+			this.first = node;
+			this.last = node;
+		} else {
+			let placeholder = this.first;
+			this.first = node;
+			this.first.next = placeholder;
+		}
 
-  }
+		this.size++;
+	}
 
-  /** pop(): remove the node from the top of the stack
+	/** pop(): remove the node from the top of the stack
    * and return its value. Should throw an error if the stack is empty. */
 
-  pop() {
+	pop() {
+		if (!this.first) throw new Error('The stack is empty.');
 
-  }
+		let placeholder = this.first;
 
-  /** peek(): return the value of the first node in the stack. */
+		if (this.first == this.last) {
+			this.last = null;
+		}
 
-  peek() {
+		this.first = this.first.next;
+		this.size -= 1;
 
-  }
+		return placeholder.val;
+	}
 
-  /** isEmpty(): return true if the stack is empty, otherwise false */
+	/** peek(): return the value of the first node in the stack. */
 
-  isEmpty() {
+	peek() {
+		return this.first.val;
+	}
 
-  }
+	/** isEmpty(): return true if the stack is empty, otherwise false */
+
+	isEmpty() {
+		return this.size === 0;
+	}
 }
 
 module.exports = Stack;
